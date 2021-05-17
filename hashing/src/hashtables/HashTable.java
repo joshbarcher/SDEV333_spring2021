ea73e794b0a3ef93418bba5017cf895c6ec42f79
Josh Archer <jarcher@greenriver.edu>
2021-05-17 10:53:04 -0700

    Added the add() method to our hash table.

--- hashing/src/hashtables/HashTable.java
+++ hashing/src/hashtables/HashTable.java
@@ -1,19 +1,25 @@
 package hashtables;
 
-public class HashTable
+import java.util.Arrays;
+
+public class HashTable<T>
 {
-    public static final int INITIAL_TABLE_SIZE = 10;
-    private Object[] table;
+    private static final int INITIAL_TABLE_SIZE = 10;
+    private static final double MAX_LOAD_FACTOR = 0.6;
+    private static final int RESIZE_RATE = 2;
+
+    private T[] table;
     private int size;
 
     public HashTable()
     {
-        table = new Object[INITIAL_TABLE_SIZE];
+        table = (T[]) new Object[INITIAL_TABLE_SIZE];
     }
 
-    public boolean add(Object element)
+    public boolean add(T element)
     {
-        if (size == table.length)
+        //check if our load factor is too large...
+        if (((double)size + 1) / table.length >= MAX_LOAD_FACTOR)
         {
             resize();
         }
@@ -21,6 +27,20 @@ public class HashTable
         //a numeric value that is generated from the properties of the Object...
         int code = Math.abs(element.hashCode());
         int index = code % table.length;
+
+        //probe to find a good position in the table
+        while (table[index] != null)
+        {
+            //check if a duplicate
+            if (table[index].equals(element))
+            {
+                return false; //don't support duplicates!
+            }
+
+            //otherwise we need to move to the next index
+            index = (index + 1) % table.length;
+        }
+
         table[index] = element;
         size++;
         return true;
@@ -28,16 +48,29 @@ public class HashTable
 
     private void resize()
     {
+        System.out.println("Table was resized");
+
         //create a new array (double the size) and copy
         //all elements over to the new array
-        Object[] oldTable = table;
-        table = new Object[oldTable.length * 2];
+        T[] oldTable = table;
+        table = (T[]) new Object[oldTable.length * RESIZE_RATE];
+
+        //set size to zero so we don't overcount when re-adding elements below
+        size = 0;
 
         for (int i = 0; i < oldTable.length; i++)
         {
-            table[i] = oldTable[i];
+            if (oldTable[i] != null)
+            {
+                //reuse our code for adding elements into the table
+                add(oldTable[i]);
+            }
         }
+    }
 
-        //done!
+    @Override
+    public String toString()
+    {
+        return Arrays.toString(table);
     }
 }
