0080a9874fe5a0ced34c1c9b2f52ba29633578ed
Josh Archer <jarcher@greenriver.edu>
2021-05-19 09:34:12 -0700

    Changed the data type in our hash table to support removal of elements.

--- hashing/src/hashtables/HashTable.java
+++ hashing/src/hashtables/HashTable.java
@@ -8,12 +8,12 @@ public class HashTable<T>
     private static final double MAX_LOAD_FACTOR = 0.6;
     private static final int RESIZE_RATE = 2;
 
-    private T[] table;
+    private HashElement[] table;
     private int size;
 
     public HashTable()
     {
-        table = (T[]) new Object[INITIAL_TABLE_SIZE];
+        table = new HashElement[INITIAL_TABLE_SIZE];
     }
 
     public boolean add(T element)
@@ -21,9 +21,55 @@ public class HashTable<T>
         //check if our load factor is too large...
         if (((double)size + 1) / table.length >= MAX_LOAD_FACTOR)
         {
-            resize();
+            resizeAndRehash();
         }
 
+        int index = findPosition(element);
+
+        //is this a duplicate?
+        if (table[index] != null)
+        {
+            return false;
+        }
+
+        table[index] = new HashElement(element);
+        size++;
+        return true;
+    }
+
+    private void resizeAndRehash()
+    {
+        System.out.println("Table was resized");
+
+        //create a new array (double the size) and copy
+        //all elements over to the new array
+        HashElement[] oldTable = table;
+        table = new HashElement[oldTable.length * RESIZE_RATE];
+
+        //set size to zero so we don't overcount when re-adding elements below
+        size = 0;
+
+        for (int i = 0; i < oldTable.length; i++)
+        {
+            if (oldTable[i] != null)
+            {
+                //reuse our code for adding elements into the table
+                add((T)oldTable[i].data);
+            }
+        }
+    }
+
+    public boolean contains(T element)
+    {
+        return table[findPosition(element)] != null;
+    }
+
+    /*
+     * This returns the index of the element if found in the table,
+     * OR the next open position to place the element in the table.
+     */
+    private int findPosition(T element)
+    {
         //a numeric value that is generated from the properties of the Object...
         int code = Math.abs(element.hashCode());
         int index = code % table.length;
@@ -34,43 +80,34 @@ public class HashTable<T>
             //check if a duplicate
             if (table[index].equals(element))
             {
-                return false; //don't support duplicates!
+                break; //found it
             }
 
             //otherwise we need to move to the next index
             index = (index + 1) % table.length;
         }
-
-        table[index] = element;
-        size++;
-        return true;
+        return index;
     }
 
-    private void resize()
+    public boolean remove(T element)
     {
-        System.out.println("Table was resized");
-
-        //create a new array (double the size) and copy
-        //all elements over to the new array
-        T[] oldTable = table;
-        table = (T[]) new Object[oldTable.length * RESIZE_RATE];
-
-        //set size to zero so we don't overcount when re-adding elements below
-        size = 0;
-
-        for (int i = 0; i < oldTable.length; i++)
-        {
-            if (oldTable[i] != null)
-            {
-                //reuse our code for adding elements into the table
-                add(oldTable[i]);
-            }
-        }
+        return false;
     }
 
     @Override
     public String toString()
     {
         return Arrays.toString(table);
+    }
+
+    private class HashElement<K>
+    {
+        private K data;
+        private boolean isRemoved;
+
+        public HashElement(K data)
+        {
+            this.data = data;
+        }
     }
 }
